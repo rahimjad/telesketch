29dd8c349d8bb7c7766f3b2d7d6045f204e76844
vagrant <vagrant@lighthouse-labs>
2015-10-22 19:33:53 +0000

    minor changes to routes

--- telesketch/app/actions.rb
+++ telesketch/app/actions.rb
@@ -1,59 +1,55 @@
-require 'base64'
-
 helpers do
 
-  def current_user 
-    unless session[:user_id].nil?
-      User.find(session[:user_id])
-    end
-  end
+ def current_user 
+   unless session[:user_id].nil?
+     User.find(session[:user_id])
+   end
+ end
 
-  def logged_in?
-    !!current_user
-  end
+ def logged_in?
+   !!current_user
+ end
 end
 
 get '/' do
-  erb :index
+ erb :index
 end
 
 get '/stories' do
-
+ @stories = Story.all
+ erb :'stories/index'
 end
 
 get '/stories/:id' do
+ @story = Story.find(params[:id])
+ erb :'stories/show'
 end
 
 get '/users/:id' do
 end
 
 get '/stories/:id/play' do
-
 end
 
 post '/stories/:id/play' do
 end
 
 post '/users/login' do
-  session[:user_id] = params[:user_id]
-  redirect '/users/:id' 
+ session[:user_id] = params[:user_id]
+ redirect '/users/:id' 
 end
 
 post '/stories/:id/rating' do
-end
-
-post '/stories/new' do 
 end
 
 post '/image/store' do
   body = request.body.read.to_s
   b64 = body.split(',')[1]#.gsub('+', ' ')
   data = Base64.decode64(b64)
+  # file = File.open('public/uploads/temp.png', 'wb')
 
-  file = File.open('public/uploads/temp.png', 'wb')
-
-  file.write(data)
-  file.close
+  # file.write(data)
+  # file.close
 
 
   "ok"
