91ca2983ebde66cdbb1cfa61048d9992564e5901
vagrant <vagrant@lighthouse-labs>
2015-10-22 00:03:16 +0000

    Migrations and Models complete

---
+++ telesketch/app/models/input.rb
@@ -0,0 +1,5 @@
+class Input < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :story
+end 
---
+++ telesketch/db/migrate/20151021234943_first_migration.rb
@@ -0,0 +1,30 @@
+class FirstMigration < ActiveRecord::Migration
+
+  def change
+    
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.timestamps
+    end
+
+    create_table :stories do |t|
+      t.string :category
+      t.timestamps
+    end
+
+    create_table :inputs do |t|
+      t.references :story
+      t.references :user
+      t.string :content
+      t.timestamps
+    end
+
+    create_table :ratings do |t|
+      t.references :story
+      t.references :user
+      t.integer :score
+      t.timestamps
+    end
+  end
+end
