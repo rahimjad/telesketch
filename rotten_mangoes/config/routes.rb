056e0b9288e3fe522bbbccaef5cda21623628845
vagrant <vagrant@lighthouse-labs>
2015-10-27 20:37:14 +0000

    reviews, sessions and users set up

---
+++ rotten_mangoes/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  root to: 'movies#index'
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  
+end
